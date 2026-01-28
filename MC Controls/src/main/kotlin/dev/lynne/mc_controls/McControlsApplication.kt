5bcc544641f3a07a13ac32fdc041ab005fb3826c
Remy Lynne <remy@lynne.dev>
2026-01-28 12:50:52 +0100

    feat: backend init

---
+++ MC Controls/src/main/kotlin/dev/lynne/mc_controls/McControlsApplication.kt
@@ -0,0 +1,11 @@
+package dev.lynne.mc_controls
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class McControlsApplication
+
+fun main(args: Array<String>) {
+    runApplication<McControlsApplication>(*args)
+}
